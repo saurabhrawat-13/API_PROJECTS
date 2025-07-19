d9644b162d55ca07859072c4dd163ca3db21ac4c
SaurabhGithub <saurabhrawat1313@gmail.com>
2025-07-19 17:20:49 +0530

    Done

---
+++ WEATHERWATCHER/src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Weather.css'
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+    <div className='search-bar'>
+    <input type="text" placeholder='Searech' />
+    <img src="" alt="" />
+
+    </div>
+    </div>
+  )
+}
+
+export default Weather
